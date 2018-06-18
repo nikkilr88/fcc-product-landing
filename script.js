12d05d43c565cc66fe829093365797c037ca1934
Nicole <nicole.sattler@live.com>
2018-06-18 17:19:54 -0500

    Refactor to use vanilla JS

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-/* global $ */
+const nav = document.querySelector('#nav-bar');
 
-$(document).ready(function(){
-	
-	$(window).scroll(function(){
-		if( $(window).scrollTop() > 50 ) {
-			$("nav").css({
-				'background': 'white', 
-				'color': '#222',
-				'border-bottom': '1px solid #ccc'
-			});
-		} else {
-			$("nav").css({
-				'background': 'none', 
-				'color': "whitesmoke",
-				'border-bottom': 'none'
-			});	
-		}
-	});
-	
-	// $(".fa-bars").on("click", function(){
-	// 	$("#content, nav").toggleClass("translateX");
-	// })
-	
+// CHANGE NAV STYLES
+window.addEventListener('scroll', function() {
+	let pos = this.scrollY;
+
+	if(pos > 50) {
+		nav.classList.add('light');
+	} 
+	else {
+		nav.classList.remove('light');
+	}
 });
